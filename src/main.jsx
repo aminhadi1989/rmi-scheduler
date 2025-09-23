2da95bcca3e0361f4593b10e4a4bc11ff424a014
aminhadi1989 <49054118+aminhadi1989@users.noreply.github.com>
2025-09-23 13:59:00 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import RMIScheduler from './RMIScheduler'
+
+const root = createRoot(document.getElementById('root'))
+root.render(<RMIScheduler />)
