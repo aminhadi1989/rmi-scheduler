1d3f467cf418c7332fecf3bca22c476c05689d15
aminhadi1989 <49054118+aminhadi1989@users.noreply.github.com>
2025-09-23 14:08:32 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import RMIScheduler from "./RMIScheduler";
-
-// Mount our app into the #root div in index.html
-const root = createRoot(document.getElementById("root"));
-root.render(<RMIScheduler />);
